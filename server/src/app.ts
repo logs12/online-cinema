f73a391dac2b26e8430fc6ef29d030d9b4798464
vsamargin <vsamargin@tsswireless.com>
2020-03-15 23:20:33 +0300

    init

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,55 @@
 import express, { Application } from 'express';
 import bodyParser from "body-parser";
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
+import errorHandler from "errorhandler";
 
 // Controllers (route handlers)
 import * as DataController from "./controllers/Data";
 import * as LogsController from "./controllers/Logs";
 
+
+// Some fake data
+const books = [
+    {
+      title: "Harry Potter and the Sorcerer's stone",
+      author: 'J.K. Rowling',
+    },
+    {
+      title: 'Jurassic Park',
+      author: 'Michael Crichton',
+    },
+  ];
+  
+  // The GraphQL schema in string form
+  const typeDefs = `
+    type Query { books: [Book] }
+    type Book { title: String, author: String }
+  `;
+  
+  // The resolvers
+  const resolvers = {
+    Query: { books: () => books },
+  };
+  
+  // Put together a schema
+  const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+  });
+
+
+
 // Create Express server
 const app: Application = express();
 
 // Express configuration
+// The GraphQL endpoint
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+  
+// GraphiQL, a visual editor for queries
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+  
 app.set("port", process.env.SERVER_PORT || 3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,6 +57,10 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
+});
+// Start the server
+app.listen(3000, () => {
+    console.log('Go to http://localhost:3000/graphiql to run queries!');
 });
 
 
